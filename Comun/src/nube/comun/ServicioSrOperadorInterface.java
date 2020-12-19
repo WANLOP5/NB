2a5df5fa09fb0361ecaefa8c807d671f09d24ad2
chaqals <chaqals@hotmail.com>
2020-12-19 20:56:46 +0100

    ServicioSrOperadorInterface añadida

--- Comun/src/nube/comun/ServicioSrOperadorInterface.java
+++ Comun/src/nube/comun/ServicioSrOperadorInterface.java
@@ -1,5 +1,17 @@
+/** ServicioSrOperadorInterface gestiona los lugares de almacenamiento del cliente
+ * y permite la bajada de ficheros del repositorio.
+ * 
+ * @author Wanderson López Veras, wan_lop05@outlook.es		
+ */
+
 package nube.comun;
 
-public interface ServicioSrOperadorInterface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
+public interface ServicioSrOperadorInterface extends Remote{
+	// Crea una carpeta para el cliente en el repositorio.
+	public boolean crearCarpetaRepositorio(int idCliente) throws RemoteException;
+	// Baja un fichero para el cliente. 
+	public void bajarFichero(String nombreFichero, int idCliente) throws RemoteException;
 }
