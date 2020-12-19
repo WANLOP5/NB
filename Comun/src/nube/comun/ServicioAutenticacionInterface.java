2b8d73af529f1985c9547aee9e70aca4b79ebec9
WANLOP5 <wan_lop05@outlook.es>
2020-12-19 02:02:44 +0100

    ServicioAutenticacionInterface añadido

--- Comun/src/nube/comun/ServicioAutenticacionInterface.java
+++ Comun/src/nube/comun/ServicioAutenticacionInterface.java
@@ -1,5 +1,29 @@
 package nube.comun;
 
-public interface ServicioAutenticacionInterface {
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ServicioAutenticacionInterface extends Remote{
+	
+	// metodo para registrar un cliente en el servidor
+	public int registrarCliente(String nombre) throws RemoteException, NotBoundException, MalformedURLException;
+	
+	// metodo para autenticar un cliente en el servidor
+	public int autenticarCliente(String nombre) throws RemoteException;
+	
+	// metodo para registrar un repositorio en el servidor
+	public int registrarRepositorio(String nombre) throws RemoteException;
+	
+	// metodo para autenticar un repositorio en el servidor
+	public int autenticarRepositorio(String nombre) throws RemoteException;
+	
+	// metodo para desconectar el cliente pasando el identificador de la sesion
+	public void desconectarCliente(int sesion) throws RemoteException;
+	
+	// metodo para desconectar el repositorio pasando el identificador de la sesion
+	public void desconectarRepositorio(int sesion) throws RemoteException;
+	
 	
 }
