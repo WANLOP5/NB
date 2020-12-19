b9524ff21ceccc01bf8642da15278988455afe8b
WANLOP5 <wan_lop05@outlook.es>
2020-12-19 19:40:23 +0100

    servicioClOperadorInterface Añadido

--- Comun/src/nube/comun/ServicioClOperadorInterface.java
+++ Comun/src/nube/comun/ServicioClOperadorInterface.java
@@ -1,9 +1,14 @@
 package nube.comun;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface ServicioClOperadorInterface extends Remote{
 	
-	
+	// metodo para subir un fichero al repositorio
+	public boolean subirFichero(Fichero fichero) throws RemoteException;
+
+	// metodo para borrar un fichero del repositorio
+	public boolean borrarFichero(String fichero,String carpeta) throws RemoteException;
 
 }
