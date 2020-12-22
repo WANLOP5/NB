2d58f8cbb33ef4beedc4cb2c00617af8272ac69f
chaqals <chaqals@hotmail.com>
2020-12-22 22:15:06 +0100

    Nuevos métodos en clase Utilidades
    Añadidos métodos iniciarRegistro y tumbarRegistro

--- Comun/src/nube/comun/Utilidades.java
+++ Comun/src/nube/comun/Utilidades.java
@@ -1,8 +1,16 @@
 package nube.comun;
 
+import java.rmi.NoSuchObjectException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.ExportException;
+import java.rmi.server.UnicastRemoteObject;
+
 public class Utilidades {
 	// Nombre de la propiedad codebase
 	public static final String CODEBASE = "java.rmi.server.codebase";
+	private static int puertoRMI = 1099;
 
 	// Añade la ruta de la clase c al codebase
 	public static void cambiarCodeBase(Class<?> c) {
@@ -19,5 +27,32 @@ public class Utilidades {
 		} 
 		
 		System.setProperty(CODEBASE, propiedadRuta);
+	}
+	
+	// Inicia el registro rmi en el sistema.
+	public static void iniciarRegistro() throws RemoteException {
+		try {
+			try { // Si el registro no se puede crear se ignora la accion por la excepcion.
+				LocateRegistry.createRegistry(1099);
+	        } catch (ExportException e) {}	    
+			
+			System.out.println("[+] REGISTRO RMI INICIALIZADO");
+		} catch(Exception e) {
+			System.err.println("(ERROR) NO SE PUDO INICIAR EL REGISTRO RMI");
+			System.exit(1);
+		}
+        
+    }
+	
+	// Tumbar el registro rmi del sistema.
+	public static void tumbarRegistro() throws RemoteException {
+		try {
+			Registry registroRMI = LocateRegistry.getRegistry(puertoRMI);
+			UnicastRemoteObject.unexportObject(registroRMI, true);
+			System.out.println("[+] REGISTRO RMI TUMBADO");
+		} catch (NoSuchObjectException e) {
+			System.err.println("(ERROR) NO SE PUDO TUMBAR EL REGISTRO RMI");
+			System.exit(1);
+		}
 	}
 }
